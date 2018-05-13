2e8e569507ba8bbd982c8b39d0d900c493746f45
Dan Hopper <dhopper2@sbcglobal.net>
2018-05-12 22:36:51 -0500

    Changed file format to mp4

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,28 +8,64 @@ let guessesLeft = 0;
 
 function loadTvShows() {
     // let obj1 = {tvShow: "The Rockford Files", link:"https://youtu.be/sQhwRr_-g50", compareText: "the rockford files"}
-    let obj1 = {tvShow: "The Rockford Files", link:"assets/images/The Rockford Files.wmv", compareText: "the rockford files"}
-    let obj2 = {tvShow: "WKRP in Cincinnati", link:"https://www.youtube.com/embed/YQvCNLIVydM", compareText: "wkrp in cincinnati"}
-    let obj3 = {tvShow: "Rawhide", link:"https://www.youtube.com/embed/E_XRfvBKEiY", compareText: "rawhide"}
+    let obj1 = {tvShow: "The Rockford Files", link:"assets/images/The Rockford Files.mp4", compareText: "the rockford files"}
+    let obj2 = {tvShow: "WKRP in Cincinnati", link:"assets/images/WKRP in Cincinnati.mp4", compareText: "wkrp in cincinnati"}
+    let obj3 = {tvShow: "Rawhide", link:"assets/images/Rawhide.mp4", compareText: "rawhide"}
+    let obj4 = {tvShow: "Flintstones", link:"assets/images/Flintstones.mp4", compareText: "Flintstones"}
+    let obj5 = {tvShow: "Gilligans Island", link:"assets/images/Gilligans Island.mp4", compareText: "gilligans island"}
+    let obj6 = {tvShow: "Hawaii Five O", link:"assets/images/Hawaii Five O.mp4", compareText: "hawaii five O"}
+    let obj7 = {tvShow: "Jeopardy", link:"assets/images/Jeopardy.mp4", compareText: "jeopardy"}
+    let obj8 = {tvShow: "Knight Rider", link:"assets/images/Knight Rider.mp4", compareText: "knight rider"}
+    let obj9 = {tvShow: "Mission Impossible", link:"assets/images/Mission Impossible.mp4", compareText: "mission impossible"}
+    let obj10 = {tvShow: "Mister Ed", link:"assets/images/Mister Ed.mp4", compareText: "mister ed"}
+    let obj11 = {tvShow: "Sanford and Son", link:"assets/images/Sanford and Son.mp4", compareText: "sanford and son"}
+    let obj12 = {tvShow: "Star Trek The Next Generation", link:"assets/images/Star Trek The Next Generation.mp4", compareText: "star trek the next generation"}
+    let obj13 = {tvShow: "The Andy Griffith Show", link:"assets/images/The Andy Griffith Show.mp4", compareText: "the andy griffith show"}
+    let obj14 = {tvShow: "The Brady Bunch", link:"assets/images/The Brady Bunch.mp4", compareText: "the brady bunch"}
+    let obj15 = {tvShow: "The Dukes of Hazzard", link:"assets/images/The Dukes of Hazzard.mp4", compareText: "the dukes of hazzard"}
+    let obj16 = {tvShow: "The Jeffersons", link:"assets/images/The Jeffersons.mp4", compareText: "the jeffersons"}
+    let obj17 = {tvShow: "The Monkees", link:"assets/images/The Monkees.mp4", compareText: "the monkees"}
+    let obj18 = {tvShow: "The Muppet Show", link:"assets/images/The Muppet Show.mp4", compareText: "the muppet show"}
+    let obj19 = {tvShow: "The Twilight Zone", link:"assets/images/The Twilight Zone.mp4", compareText: "the twilight zone"}
+    let obj20 = {tvShow: "Welcome Back Kotter", link:"assets/images/Welcome Back Kotter.mp4", compareText: "welcome back kotter"}
     
     tvShows.push(obj1);
     tvShows.push(obj2);
     tvShows.push(obj3);
-    document.getElementById("hangmanPic").style.visibility = "visible";
-    document.getElementById("hangmanIframe").style.display = "none";
+    tvShows.push(obj4);
+    tvShows.push(obj5);
+    tvShows.push(obj6);
+    tvShows.push(obj7);
+    tvShows.push(obj8);
+    tvShows.push(obj9);
+    tvShows.push(obj10);
+    tvShows.push(obj11);
+    tvShows.push(obj12);
+    tvShows.push(obj13);
+    tvShows.push(obj14);
+    tvShows.push(obj15);
+    tvShows.push(obj16);
+    tvShows.push(obj17);
+    tvShows.push(obj18);
+    tvShows.push(obj19);
+    tvShows.push(obj20);
+    
     document.addEventListener("keydown", processGuess);
     resetVars();
 }
 
 function resetVars () {
+    document.getElementById("hangmanIframe").src = "#";
+    document.getElementById("hangmanIframe").style.display = "none";
+    document.getElementById("hangmanPic").style.display = "block";
     document.getElementById("wins").innerHTML="wins:" + wins;
-    tvShow = tvShows[Math.floor((Math.random() * 3))];
+    tvShow = tvShows[Math.floor((Math.random() * tvShows.length))];
     document.getElementById("tvTitle").innerHTML=siftTvShow();
     document.getElementById("numberOfGuessesLeft").innerHTML = "Number of guesses left:" + guessesLeft;
     document.getElementById("letterGuessed").innerHTML = ""; 
     document.getElementById("info").innerHTML = "Press any letter for your first guess.";  
     document.getElementById("guessesSoFar").innerHTML = "";  
-    guessesSoFar = [];   
+    guessesSoFar = [];
 }
 
 function siftTvShow() {
@@ -48,7 +84,7 @@ function siftTvShow() {
 
 function processGuess(e) {    
     if(alpha.indexOf(e.key.toLowerCase()) < 0){
-        alert("Character " + e.key + " is not a valid key. Try again.")
+        document.getElementById("info").innerHTML = "Character " + e.key + " is not a valid key. Try again.";
         return;
     }
     if(guessesSoFar.indexOf(e.key.toLowerCase()) > -1) {
@@ -98,4 +134,8 @@ function isGameOver() {
     document.getElementById("hangmanIframe").src = tvShow.link;
     wins++;
     document.getElementById("wins").innerHTML = "wins:" + wins;
+}
+
+function newGame () {
+    resetVars();
 }
